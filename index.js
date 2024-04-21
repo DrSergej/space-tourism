747d606c87b42d1854baca37b86c87d86a7d560b
DrSergej <sergejdr.33@gmail.com>
2024-04-21 20:44:41 +0200

    turning-helper-functions-to-be-universal

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 "use strict";
 
 // fetching .json file
-let jsonData;
+let destinationsData;
+let crewData;
+let technologyData;
 
 fetch("data.json")
 	.then((response) => response.json())
 	.then((data) => {
-		jsonData = data.destinations;
-		// console.log(jsonData);
+		destinationsData = data.destinations;
+		crewData = data.crew;
+		technologyData = data.technology;
 	})
 	.catch((error) => {
 		console.error("Error fetching data:", error);
 	});
 
-// mobile-navigation
+// ### mobile-navigation ###
 const navButton = document.querySelector(".mobile-nav-toggle");
 const navMenu = document.querySelector(".primary-navigation");
 
@@ -31,7 +34,7 @@ navButton.addEventListener("click", () => {
 });
 // #######################################################################
 
-// tabs
+// ### tabs ###
 const tabHeading = document.querySelector(".destination-info h2");
 const tabDescription = document.querySelector(".destination-info p");
 const tabMeta1 = document.querySelector(".meta-1 p");
@@ -40,40 +43,48 @@ const htmlImg = document.querySelector(".grid-container--destination img");
 const htmlAltImg = document.querySelector(
 	".grid-container--destination source"
 );
-const allTabs = document.querySelector(".tab-list");
+const allTabs = Array.from(document.querySelector(".tab-list").children);
 
 // adding event listener to every tab
-Array.from(allTabs.children).forEach((tab) => {
+allTabs.forEach((tab) => {
 	tab.addEventListener("click", function () {
-		// console.log(tab.innerText.toLowerCase());
-		loadContent(tab.innerText.toLowerCase());
+		resetActive(allTabs);
+		loadContent(destinationsData, tab.innerText.toLowerCase());
+		tab.setAttribute("aria-selected", true);
 	});
 });
 
-// helper functions
-const findContent = function (destination) {
-	const found = jsonData.find((el) => el.name.toLowerCase() === destination);
-	console.log("FOUND", found);
+// ### helper functions ###
+
+// set aria-selected property to false on all items (tabs, dots, numbers)
+const resetActive = function (items) {
+	items.forEach((item) => {
+		item.setAttribute("aria-selected", false);
+	});
+};
+
+// list = all data to search e.g. crewData, technologyData... target = data you looking for e.g. moon, mars,...
+const findContent = function (list, target) {
+	const found = list.find((el) => el.name.toLowerCase() === target);
+	// console.log("FOUND", found);
 	return found;
 };
 
-const loadContent = function (destination) {
-	const contentData = findContent(destination);
+// list = all data to search e.g. crewData, technologyData... target = data you looking for e.g. moon, mars,...
+const loadContent = function (list, target) {
+	const contentData = findContent(list, target);
 	tabHeading.innerText = contentData.name;
 	tabDescription.innerText = contentData.description;
 	tabMeta1.innerText = contentData.distance;
 	tabMeta2.innerText = contentData.travel;
 	htmlImg.setAttribute("src", contentData.images.png);
-	htmlImg.setAttribute("alt", destination);
+	htmlImg.setAttribute("alt", target);
 	htmlAltImg.setAttribute("srcset", contentData.images.webp);
 };
 // #######################################################################
 
-// !TODO: Children nach HTML inhalt sucken in der data.json und Inhalt austauschen
+// ### dots ###
 // #######################################################################
 
-// dots
-// #######################################################################
-
-// numbers
+// ### numbers ###
 // #######################################################################
